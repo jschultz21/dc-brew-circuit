eebabe28f00b176de71c6a925094977913c95279
Julia Schultz <julia.schultz91@gmail.com>
2016-07-01 15:42:06 -0400

    brewery view pages set up and beer new page ready to build

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,7 +1,19 @@
-class BreweriesController < ApplicationController
+class BeersController < ApplicationController
 
   def show
     @brewery = Brewery.find(params[:id])
-    @beers = @brewery.Beer.all
+    @beers = @brewery.beers.all
   end
+
+  def new
+    @brewery = Brewery.find(params[:brewery_id])
+    @beer = Beer.create!(beer_params)
+  end
+
+end
+
+
+
+private
+def beer_params
 end
--- db/beer_data.rb
+++ db/beer_data.rb
@@ -1,110 +1,110 @@
 def get_beer_data
   beer_data = {
     "denizens" =>  [{
-      name: 'Denizens Beer 1',
+      name: 'Dubplate American Double IPA',
       style: 'IPA',
       photo_url: 'url'
       },
       {
-      name: 'Denizens Beer 2',
+      name: 'Whodunnit? Sour Red Saison',
       style: 'IPA',
       photo_url: 'url'
         }],
 
     "dc_brau" =>  [{
-      name: 'The Citizen',
+      name: 'The Corruption',
       style: 'IPA',
       photo_url: 'url'
         },
         {
       name: 'The Public',
-      style: 'IPA',
+      style: 'American Pale Ale',
       photo_url: 'url'
           }],
     "three_stars" =>  [{
-      name: '3 stars beer 1',
-      style: 'IPA',
+      name: 'Peppercorn Saison',
+      style: 'Saison',
       photo_url: 'url'
         },
         {
-      name: '3 stars beer 2',
-      style: 'IPA',
+      name: 'Ghost',
+      style: 'White IPA',
       photo_url: 'url'
           }],
 
     "hellbender" =>  [{
-      name: 'hellbender beer 1',
-      style: 'IPA',
+      name: 'North x Northeast Coffee Stout',
+      style: 'Stout',
       photo_url: 'url'
         },
         {
-      name: 'hellbender beer 2',
-      style: 'IPA',
+      name: 'Bare Bones',
+      style: 'Kölsch',
       photo_url: 'url'
           }],
 
     "atlas" =>  [{
       name: 'District Common',
-      style: 'IPA',
+      style: 'Calfornia Common',
       photo_url: 'url'
         },
         {
-      name: 'Ponzi',
-      style: 'IPA',
+      name: 'NSFW',
+      style: 'Imperial Double Black IPA',
       photo_url: 'url'
           }],
 
     "bardo" =>  [{
-      name: 'bardo beer 1',
-      style: 'IPA',
+      name: 'Marion Berry',
+      style: 'Lambic',
       photo_url: 'url'
         },
         {
-      name: 'bardo beer 2',
+      name: 'Smoked and Oaked Bacon IPA',
       style: 'IPA',
       photo_url: 'url'
           }],
 
     "right_proper_shaw" =>  [{
-      name: 'RPS beer 1',
-      style: 'IPA',
+      name: 'BOSCO',
+      style: 'American Tart Ale',
       photo_url: 'url'
         },
         {
-      name: 'RPS beer 2',
-      style: 'IPA',
+      name: 'Raised by Wolves',
+      style: 'Aromatic Pale Ale',
       photo_url: 'url'
           }],
     "port_city" =>  [{
-      name: 'port city beer 1',
-      style: 'IPA',
+      name: 'Derecho',
+      style: 'Common',
       photo_url: 'url'
         },
         {
-      name: 'port city beer 2',
-      style: 'IPA',
+      name: 'Port City Porter',
+      style: 'Porter',
       photo_url: 'url'
           }],
 
     "bluejacket" =>  [{
-      name: 'bj beer 1',
-      style: 'IPA',
+      name: 'Mexican Radio',
+      style: 'Spiced Sweet Stout',
       photo_url: 'url'
         },
         {
-      name: 'bj beer 2',
-      style: 'IPA',
+      name: 'Rude Mechanical',
+      style: 'Hoppy Lager',
       photo_url: 'url'
           }],
 
     "right_proper_brookland" =>  [{
-      name: 'Right Proper beer 1',
-      style: 'IPA',
+      name: 'White Bicycles',
+      style: 'Rustic Witbier',
       photo_url: 'url'
           },
           {
-      name: 'Right Proper beer 2',
-      style: 'IPA',
+      name: 'Diamonds, Fur Coat, Champagne',
+      style: 'Berliner Weisse',
       photo_url: 'url'
               }]
             }
